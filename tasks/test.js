65cc36394a722a9d1497c76bd2ec50e3d096b7a7
Pablo Cabrera <pablo.cabrera.munoz@gmail.com>
2015-04-12 17:00:25 -0300

    Added support for VBoxSeleniumEnvironment

--- tasks/test.js
+++ tasks/test.js
@@ -9,21 +9,10 @@ module.exports = function(grunt) {
     var path = require("path");
 
     var gabarito = require(root + "/gabarito");
+    var plumbing = gabarito.plumbing;
 
-    var createSeleniumEnv = function (browser) {
-        return new gabarito.runner.SeleniumEnvironment(browser,
-            "http://localhost:4444/wd/hub",
-            "20.0.0.103");
-//            "192.168.0.165");
-    };
-
-    grunt.registerMultiTask("test", "gabarito test runner", function() {
-
-        var done = this.async();
-        var runner = new gabarito.runner.Runner();
-        var cwd = process.cwd();
-
-        this.files.forEach(function (f) {
+    var setupFiles = function (runner, files) {
+        files.forEach(function (f) {
             f.src.forEach(function (f) {
                 if (f.indexOf("/") !== 0) {
                     f = path.join(cwd, f);
@@ -31,26 +20,31 @@ module.exports = function(grunt) {
                 runner.addFile(f);
             });
         });
+    };
+
+    var setupEnvironments = function (runner) {
+        var node = new plumbing.NodeEnvironment(gabarito);
+        runner.addEnvironment(node);
+    };
+
+    var setupReporters = function (runner) {
+        runner.addReporter(new plumbing.ConsoleReporter());
+
+        var jUnitReporter = new plumbing.JUnitXmlReporter(
+                path.join(cwd, "/test/result/results.xml"));
+        runner.addReporter(jUnitReporter);
+
+    };
+
+    grunt.registerMultiTask("test", "gabarito test runner", function() {
+        var done = this.async();
+        var runner = new plumbing.Runner();
 
         grunt.file.mkdir(path.join(cwd, "/test/result"));
 
-        var nodeEnv = new gabarito.runner.NodeEnvironment(gabarito);
-        runner.addEnvironment(nodeEnv);
-
-        var firefoxSeleniumEnv = createSeleniumEnv("firefox");
-        var chromeSeleniumEnv = createSeleniumEnv("chrome");
-        var ieSeleniumEnv = createSeleniumEnv("internet explorer");
-
-//        runner.addEnvironment(firefoxSeleniumEnv);
-//        runner.addEnvironment(chromeSeleniumEnv);
-        runner.addEnvironment(ieSeleniumEnv);
-
-        var consoleReporter = new gabarito.runner.ConsoleReporter();
-        runner.addReporter(consoleReporter);
-
-        var jUnitReporter = new gabarito.runner.JUnitXmlReporter(
-                path.join(cwd, "/test/result/results.xml"));
-        runner.addReporter(jUnitReporter);
+        setupFiles(runner, this.files);
+        setupEnvironments(runner);
+        setupReporters(runner);
 
         runner.run(function (results) {
             var hasErrors = results.some(function (r) {
